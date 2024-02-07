0375494280ab9cc23c38cd3eefce1511f71b6d40
Kevin Roan <kevinroan@proton.me>
2024-02-07 22:39:27 +0530

    razorpay integration:

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -20,34 +20,70 @@ const Form = () => {
     e.preventDefault();
     console.log("Form submitted with data:", formData);
   };
+  const handlePayment = () => {
+    var options = {
+      key: "rzp_test_smoqKU73vlbB3h",
+      key_secret: "TBZEpNqqYgQoRv3fAXPMUSY5",
+      amount: 100000,
+      currency: "INR",
+      name: "ADACODE_SOLUTIONS",
+      description: "15 Days Master Class Program",
+      handler: function (response) {
+        alert(response.razorpay_payment_id);
+      },
+      prefill: {
+        name: formData.fullName,
+        email: formData.email,
+        contact: formData.mobileNumber,
+      },
+      notes: {
+        address: "Razorpay Corporate office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    var pay = new window.Razorpay(options);
+    pay.open();
+  };
+
   return (
     <div>
       <section className="form_contianer">
         <h5>--- Fill up your details ---</h5>
         <form onSubmit={handleSubmit}>
-          <label>Full Name*</label>
+          <label>
+            Full Name<span className="aster"> *</span>
+          </label>
           <input
             type="text"
             name="fullName"
             placeholder="Enter your name"
             onChange={handleInputChange}
             value={formData.fullName}
+            required
           />
-          <label>Mobile Number*</label>
+          <label>
+            Mobile Number<span className="aster"> *</span>
+          </label>
           <input
-            type="text"
+            type="tel"
             name="mobileNumber"
             placeholder="Mobile Number"
             onChange={handleInputChange}
             value={formData.mobileNumber}
+            required
           />
-          <label>Email *</label>
+          <label>
+            Email <span className="aster">*</span>
+          </label>
           <input
             type="text"
             name="email"
             placeholder="Email"
             onChange={handleInputChange}
             value={formData.email}
+            required
           />
           <label>Qualification</label>
           <select
@@ -61,7 +97,7 @@ const Form = () => {
             <option>Masteres</option>
             <option>Other</option>
           </select>
-          <button>Apply Now</button>
+          <button onClick={handlePayment}>Apply Now</button>
         </form>
       </section>
     </div>
