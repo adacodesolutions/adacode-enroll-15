3f4c50223427d047aa0f4c33712498d46127b755
Kevin Roan <kevinroan@proton.me>
2024-03-14 02:39:12 +0530

    minor update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,11 +10,8 @@ const Home = () => {
           Ada<span>code </span>Solu<span>tions</span>
         </h1>
         <p className="quote">Excellence for tomorrows Innovators</p>
-        <p className="details">15 Days Master Class for just 1000RS..!</p>
+        <p className="date_button">15 Days Master Class for just 1000RS..!</p>
         <div className="hero_details">
-          <div className="date_button">
-            <h5>March 9th 2024</h5>
-          </div>
           <p>3rd Floor, 4 Wing Avenue, Panniyankara</p>
         </div>
       </section>
