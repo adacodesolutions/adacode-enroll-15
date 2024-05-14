3ffb49647f45b78e1601cdef45cb94063d303d8d
Kevin Roan <kevinroan@proton.me>
2024-05-14 15:43:10 +0530

    regenerated api keys

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log("firebaeconfig", firebaseConfig);
-
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
